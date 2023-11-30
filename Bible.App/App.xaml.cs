0333d724a4d06c375f5cd4bab4a82260ad11ed8a
Daniel Collingwood <82693586+danzuep@users.noreply.github.com>
2023-11-30 19:18:22 +0800

    removed the last vestiges of modularity, but there's still a memory leak

--- Bible.App/App.xaml.cs
+++ Bible.App/App.xaml.cs
@@ -1,27 +1,10 @@
-﻿using BibleApp.ViewModels;
-using CommunityToolkit.Mvvm.DependencyInjection;
-
-namespace BibleApp
+﻿namespace BibleApp
 {
     public partial class App : Application
     {
-        private bool _initialized;
-
         public App()
         {
             InitializeComponent();
-
-            // Register services
-            if (!_initialized)
-            {
-                _initialized = true;
-                Ioc.Default.ConfigureServices(
-                    new ServiceCollection()
-                    //ViewModels
-                    .AddTransient<MainPageViewModel>()
-                    .BuildServiceProvider());
-            }
-
             MainPage = new AppShell();
         }
     }
--- Bible.App/Pages/MainPage.xaml.cs
+++ Bible.App/Pages/MainPage.xaml.cs
@@ -1,12 +1,35 @@
-﻿using BibleApp.ViewModels;
-
-namespace BibleApp.Pages
+﻿namespace BibleApp.Pages
 {
-    public sealed partial class MainPage : BasePage<MainPageViewModel>
+    public sealed partial class MainPage : ContentPage
     {
         public MainPage() : base()
         {
             InitializeComponent();
+            BindingContext = this;
+            if (string.IsNullOrWhiteSpace(Title))
+                Title = GetType().Name;
+            chapterPicker.ItemsSource = Enumerable.Range(1, 150).ToArray();
+            chapterPicker.SelectedIndex = 0;
+            Verses = LoadVerses(1);
+        }
+
+        public IReadOnlyList<string> Verses { get; private set; }
+
+        private IReadOnlyList<string> LoadVerses(int chapterNumber)
+        {
+            int verseCount = Random.Shared.Next(2, 176);
+            var verses = Enumerable.Range(1, verseCount)
+                .Select(v => $"Chapter #{chapterNumber}, Verse #{v}.");
+            return verses.ToArray();
+        }
+
+        private void OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (sender is Picker picker && picker.SelectedItem is int chapter)
+            {
+                Verses = LoadVerses(chapter);
+                OnPropertyChanged(nameof(Verses));
+            }
         }
     }
 }
