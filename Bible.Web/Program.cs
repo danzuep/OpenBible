27829bb6b74dca324df1432f5fc2a052adb1d0a4
Daniel Collingwood <82693586+danzuep@users.noreply.github.com>
2024-01-24 14:22:24 +0800

    verse of the day

--- Bible.Web/Program.cs
+++ Bible.Web/Program.cs
@@ -13,13 +13,6 @@ namespace Bible.Web
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddOidcAuthentication(options =>
-            {
-                // Configure your authentication provider options here.
-                // For more information, see https://aka.ms/blazor-standalone-auth
-                builder.Configuration.Bind("Local", options.ProviderOptions);
-            });
-
             await builder.Build().RunAsync();
         }
     }
