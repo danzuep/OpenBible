a1b33eabe899c62670fd05da025fea35323cd6e1
Daniel Collingwood <82693586+danzuep@users.noreply.github.com>
2025-02-19 09:36:51 +0800

    Updated to net9.0

--- Scripts/_version.ps1
+++ Scripts/_version.ps1
@@ -2,8 +2,8 @@
 # @('bin','obj') | ForEach-Object { Get-ChildItem -Recurse -Directory -Filter $_ -Force | ForEach-Object { Write-Host "Deleting folder: $($_.FullName)"; Remove-Item $($_.FullName) -Recurse -Force -ErrorAction SilentlyContinue } }; # dotnet nuget locals all --clear
 
 $projectName = "Bible";
-$buildVersion = "0.1.3";
-$dotnetTarget = "net8.0";
+$buildVersion = "0.2.0";
+$dotnetTarget = "net9.0";
 $configuration = "Release";
 $publishOutputFolder = "publish";
 $projectAppName = "${projectName}.App";
