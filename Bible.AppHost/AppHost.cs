57107762b0337e0c5f889cd8f81423c07059a725
Daniel Collingwood <82693586+danzuep@users.noreply.github.com>
2025-08-02 01:38:17 +0800

    Added Bible.Web as a Blazor Aspire app

---
+++ Bible.AppHost/AppHost.cs
@@ -0,0 +1,12 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.Bible_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health");
+
+builder.AddProject<Projects.Bible_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
